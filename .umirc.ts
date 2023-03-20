eae1c28018b1173a7a2b9a9f625b4d14f71d770a
wb-yzg904416@antgroup.com <wb-yzg904416@antgroup.com>
2023-03-20 10:33:35 +0800

    fix: 优化

--- .umirc.ts
+++ .umirc.ts
@@ -10,6 +10,10 @@ export default defineConfig({
   request: {},
   layout: false,
   hash: true,
+  theme: {
+    "@border-radius-base": "4px",
+    "@primary-color": "#1677ff",
+  },
   // base: '/geojson-viewer',
   // publicPath: '/geojson-viewer/',
   routes: [
