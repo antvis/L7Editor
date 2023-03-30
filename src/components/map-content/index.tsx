52f81e3bc28f219074bb64b351d1e4a1397d75f2
syb01094648 <syb01094648@antgroup.com>
2023-03-30 16:53:27 +0800

    fix: 修改title

--- src/components/map-content/index.tsx
+++ src/components/map-content/index.tsx
@@ -77,7 +77,7 @@ export const MapContent: React.FC = () => {
               onClick={saveEditorText}
             ></Button>
           </Tooltip>
-          <Tooltip trigger="hover" placement="left" overlay="初始化数据">
+          <Tooltip trigger="hover" placement="left" overlay="重置数据">
             <Button
               icon={<ClearOutlined />}
               onClick={() => {
