e88db1c5622bbef469773359e62c4c563fa4259e
syb01094648 <syb01094648@antgroup.com>
2023-05-31 18:17:43 +0800

    feat: 帮助文档恢复

--- src/components/map-content/btn/handback-btn.tsx
+++ src/components/map-content/btn/handback-btn.tsx
@@ -1,11 +1,13 @@
 import { QuestionCircleOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import { Button, Tooltip } from 'antd';
 
 export default () => {
   const onHandback = () => {
-    window.open('https://www.yuque.com/antv/l7/fwwdndml8e0gllp2', '_blank');
+    window.open('https://www.yuque.com/antv/l7/buhith8khkc9mfsg?singleDoc#', '_blank');
   };
   return (
+    <Tooltip title="帮助文档">
     <Button icon={<QuestionCircleOutlined />} onClick={onHandback}></Button>
+    </Tooltip>
   );
 };
