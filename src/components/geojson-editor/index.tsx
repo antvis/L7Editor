3e74542da3189c150a10f3d596b33e5d9a2f48ef
suyubin <84109842+websybin@users.noreply.github.com>
2023-10-23 14:26:31 +0800

    fix: 修复初始化页面两次触发bbox问题 (#5)
    * fix: 修复初始化页面两次出发bbox问题
    
    * fix: 命名规范修改
    
    * fix: useDebounce命名规范修改
    
    ---------
    
    Co-authored-by: syb01094648 <syb01094648@antgroup.com>

--- src/components/geojson-editor/index.tsx
+++ src/components/geojson-editor/index.tsx
@@ -1,4 +1,4 @@
-import { useDebounceEffect, useMount, useSize } from 'ahooks';
+import { useDebounce, useMount, useSize, useUpdateEffect } from 'ahooks';
 import type { editor } from 'monaco-editor';
 import * as monacoEditor from 'monaco-editor/esm/vs/editor/editor.api';
 import React, {
@@ -31,7 +31,8 @@ export const GeoJsonEditor: React.FC<EditorProps> = forwardRef((props, ref) => {
   const [container, setContainer] = useState<HTMLDivElement | null>(null);
   const { width = 0, height = 0 } = useSize(container) ?? {};
   const styles = useStyle();
-  const [editorTextChange, setEditorTextChange] = useState('');
+  const [geoJonText, setGeoJonText] = useState('');
+  const debouncedGeoJsonText = useDebounce(geoJonText, { wait: 2000 });
   const { t } = useTranslation();
 
   // document format
@@ -82,7 +83,7 @@ export const GeoJsonEditor: React.FC<EditorProps> = forwardRef((props, ref) => {
   const monacoChange = (event: string) => {
     if (language === 'json') {
       setEditorText(event);
-      setEditorTextChange(event);
+      setGeoJonText(event);
       return;
     }
     setScriptContent?.(event);
@@ -113,16 +114,12 @@ export const GeoJsonEditor: React.FC<EditorProps> = forwardRef((props, ref) => {
     [scriptContent, t],
   );
 
-  useDebounceEffect(
-    () => {
-      const features = saveEditorText();
-      if (autoFitBounds) {
-        bboxAutoFit(features);
-      }
-    },
-    [editorTextChange],
-    { wait: 2000 },
-  );
+  useUpdateEffect(() => {
+    const features = saveEditorText();
+    if (autoFitBounds) {
+      bboxAutoFit(features);
+    }
+  }, [debouncedGeoJsonText]);
 
   const value = useMemo(() => {
     if (language === 'javascript') {
