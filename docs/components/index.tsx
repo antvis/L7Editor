46d1fa91c432e6def618d0ea28d83c58db823a09
yanxiong <oujinhui.ojh@antgroup.com>
2023-11-12 18:53:28 +0800

    fix: 删除无用代码

--- docs/components/index.tsx
+++ docs/components/index.tsx
@@ -3,14 +3,6 @@ import React from 'react';
 import './index.less';
 
 export default () => {
-  // const { loading, data: isChina = true } = useRequest(async () => {
-  //   const { short_name: countryCode } = await fetch(
-  //     'https://ip.useragentinfo.com/json',
-  //   ).then((res) => res.json());
-
-  //   return countryCode === 'CN';
-  // });
-
   return (
     <div className="editor">
       <L7Editor activeTab="geojson" />
