ca563d22cb8ee151ff38e237e3cd8aa9be6ed66b
syb01094648 <syb01094648@antgroup.com>
2023-11-27 15:39:11 +0800

    fix: 完善代码

--- docs/components/index.tsx
+++ docs/components/index.tsx
@@ -5,7 +5,7 @@ import './index.less';
 export default () => {
   return (
     <div className="editor">
-      <L7Editor activeTab="geojson" wasmPath='https://renderofficepre.alipayobjects.com/p/yuyan/180020010001254664/'/>
+      <L7Editor activeTab="geojson" />
     </div>
   );
 };
