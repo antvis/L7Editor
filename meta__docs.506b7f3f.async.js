696809a336eff05878feafdabbb285e767defcfa
heiyexing <496845051@qq.com>
2024-05-31 09:10:10 +0800

    Updates

---
+++ meta__docs.506b7f3f.async.js
@@ -0,0 +1,78 @@
+"use strict";(self.webpackChunk_antv_l7_editor=self.webpackChunk_antv_l7_editor||[]).push([[904],{11e3:function(o,e,a){a.r(e),a.d(e,{demos:function(){return p}});var n=a(15009),t=a.n(n),l=a(99289),u=a.n(l),I=a(67294),p={"docs-docs-demo-docs":{component:I.memo(I.lazy(function(){return a.e(433).then(a.bind(a,97079))})),asset:{type:"BLOCK",id:"docs-docs-demo-docs",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:a(27786).Z},"@antv/l7-editor":{type:"NPM",value:"1.1.13"},react:{type:"NPM",value:"18.1.0"}},entry:"index.tsx"},context:{"@antv/l7-editor":a(31252),react:a(67294)},renderOpts:{compile:function(){var c=u()(t()().mark(function s(){var r,x=arguments;return t()().wrap(function(d){for(;;)switch(d.prev=d.next){case 0:return d.next=2,a.e(19).then(a.bind(a,4019));case 2:return d.abrupt("return",(r=d.sent).default.apply(r,x));case 3:case"end":return d.stop()}},s)}));function v(){return c.apply(this,arguments)}return v}()}}}},49878:function(o,e,a){a.r(e),a.d(e,{demos:function(){return t}});var n=a(67294),t={}},15561:function(o,e,a){a.r(e),a.d(e,{demos:function(){return p}});var n=a(15009),t=a.n(n),l=a(99289),u=a.n(l),I=a(67294),p={"docs-demo-components":{component:I.memo(I.lazy(function(){return a.e(433).then(a.bind(a,32127))})),asset:{type:"BLOCK",id:"docs-demo-components",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:a(323).Z},"@antv/l7-editor":{type:"NPM",value:"1.1.13"},react:{type:"NPM",value:"18.1.0"},"./index.less":{type:"FILE",value:a(34676).Z}},entry:"index.tsx"},context:{"@antv/l7-editor":a(31252),react:a(67294),"./index.less":a(29972)},renderOpts:{compile:function(){var c=u()(t()().mark(function s(){var r,x=arguments;return t()().wrap(function(d){for(;;)switch(d.prev=d.next){case 0:return d.next=2,a.e(19).then(a.bind(a,4019));case 2:return d.abrupt("return",(r=d.sent).default.apply(r,x));case 3:case"end":return d.stop()}},s)}));function v(){return c.apply(this,arguments)}return v}()}}}},29972:function(o,e,a){a.r(e)},83907:function(o,e,a){a.r(e),a.d(e,{texts:function(){return n}});const n=[{value:"\u8BE5\u6587\u6863\u4E3B\u8981\u8BF4\u660E\u5C06 L7 Editor \u4F5C\u4E3A React \u7EC4\u4EF6\u5728\u9879\u76EE\u4E2D\u4F7F\u7528\u7684 API\u3002",paraId:0,tocIndex:0},{value:"\u6CE8\u610F\u5728\u4F7F\u7528 ",paraId:1,tocIndex:0},{value:"<L7Editor />",paraId:1,tocIndex:0},{value:" \u4E4B\u524D\u8BF7\u4FDD\u8BC1\u73AF\u5883\u4E2D\u5DF2\u5B89\u88C5 ",paraId:1,tocIndex:0},{value:'"@antv/l7": "^2.0.0"',paraId:1,tocIndex:0},{value:" \u548C ",paraId:1,tocIndex:0},{value:'"antd": "^5.0.0"',paraId:1,tocIndex:0},{value:"\u3002",paraId:1,tocIndex:0},{value:"\u9ED8\u8BA4\u793A\u4F8B",paraId:2,tocIndex:1},{value:"\u914D\u7F6E\u9879",paraId:3},{value:"\u914D\u7F6E L7Editor \u521D\u59CB\u5316\u9ED8\u8BA4\u914D\u7F6E",paraId:4,tocIndex:3},{value:"\u5C5E\u6027",paraId:5,tocIndex:3},{value:"\u63CF\u8FF0",paraId:5,tocIndex:3},{value:"\u7C7B\u578B",paraId:5,tocIndex:3},{value:"\u9ED8\u8BA4\u503C",paraId:5,tocIndex:3},{value:"baseMap",paraId:5,tocIndex:3},{value:"\u5730\u56FE\u9009\u9879",paraId:5,tocIndex:3},{value:"\u2018Gaode\u2019\uFF5C'Mapbox'",paraId:5,tocIndex:3},{value:"Gaode",paraId:5,tocIndex:3},{value:"mapOption",paraId:5,tocIndex:3},{value:"\u5730\u56FE\u914D\u7F6E \u8BE6\u89C1 ",paraId:5,tocIndex:3},{value:"MapOptions",paraId:5,tocIndex:3},{value:"MapOptions",paraId:5,tocIndex:3},{value:"-",paraId:5,tocIndex:3},{value:"primaryColor",paraId:5,tocIndex:3},{value:"\u56FE\u5C42\u989C\u8272",paraId:5,tocIndex:3},{value:"string",paraId:5,tocIndex:3},{value:"#1677ff",paraId:5,tocIndex:3},{value:"rightPanelWidth",paraId:5,tocIndex:3},{value:"\u53F3\u4FA7\u8FB9\u680F\u5BBD\u5EA6",paraId:5,tocIndex:3},{value:"number",paraId:5,tocIndex:3},{value:"40",paraId:5,tocIndex:3},{value:"autoFitBounds",paraId:5,tocIndex:3},{value:"\u662F\u5426\u81EA\u52A8\u8C03\u6574\u8FB9\u754C",paraId:5,tocIndex:3},{value:"boolean",paraId:5,tocIndex:3},{value:"true",paraId:5,tocIndex:3},{value:"popupTrigger",paraId:5,tocIndex:3},{value:"\u56FE\u5C42 popup \u89E6\u53D1\u65B9\u5F0F",paraId:5,tocIndex:3},{value:"'click'\uFF5C'hover'",paraId:5,tocIndex:3},{value:"click",paraId:5,tocIndex:3},{value:"activeTab",paraId:5,tocIndex:3},{value:"\u4FA7\u9762\u677F\u5C55\u793A",paraId:5,tocIndex:3},{value:"'geojson'\uFF5C'table'\uFF5C'wkt' \uFF5C string",paraId:5,tocIndex:3},{value:"geojson",paraId:5,tocIndex:3},{value:"features",paraId:5,tocIndex:3},{value:"\u521D\u59CB\u5316\u6570\u636E",paraId:5,tocIndex:3},{value:" Feature[]",paraId:5,tocIndex:3},{value:"[]",paraId:5,tocIndex:3},{value:"officialLayers",paraId:5,tocIndex:3},{value:"\u5B98\u65B9\u56FE\u5C42\u9009\u62E9",paraId:5,tocIndex:3},{value:"string[]",paraId:5,tocIndex:3},{value:"[]",paraId:5,tocIndex:3},{value:"theme",paraId:5,tocIndex:3},{value:"\u4E3B\u9898\u914D\u7F6E",paraId:5,tocIndex:3},{value:"'light' \uFF5C 'dark'",paraId:5,tocIndex:3},{value:"'light'",paraId:5,tocIndex:3},{value:"locale",paraId:5,tocIndex:3},{value:"\u8BED\u8A00\u8BBE\u7F6E",paraId:5,tocIndex:3},{value:"'zh-CN' \uFF5C 'en-US'",paraId:5,tocIndex:3},{value:"'zh-CN'",paraId:5,tocIndex:3},{value:"coordConvert",paraId:5,tocIndex:3},{value:"\u6570\u636E\u8F6C\u6362",paraId:5,tocIndex:3},{value:"'GCJ02'\uFF5C'WGS84'",paraId:5,tocIndex:3},{value:"GCJ02",paraId:5,tocIndex:3},{value:"mapControl",paraId:5,tocIndex:3},{value:"\u63A7\u4EF6\u663E\u9690",paraId:5,tocIndex:3},{value:"MapControlProps",paraId:6,tocIndex:3},{value:"-",paraId:5,tocIndex:3},{value:"toolbar",paraId:5,tocIndex:3},{value:"\u5934\u90E8\u7EC4\u4EF6\u663E\u9690",paraId:5,tocIndex:3},{value:"ToolbarProps",paraId:7,tocIndex:3},{value:"-",paraId:5,tocIndex:3},{value:"tabItems",paraId:5,tocIndex:3},{value:"\u4FA7\u9762\u7248\u6807\u7B7E\u9875\u9009\u9879\u5361\u5185\u5BB9",paraId:5,tocIndex:3},{value:"TabItemType",paraId:5,tocIndex:3},{value:"-",paraId:5,tocIndex:3},{value:"showTextLayer",paraId:5,tocIndex:3},{value:"\u662F\u5426\u5C55\u793A\u5143\u7D20\u6587\u672C",paraId:5,tocIndex:3},{value:"boolean",paraId:5,tocIndex:3},{value:"false",paraId:5,tocIndex:3},{value:"textLayerFields",paraId:5,tocIndex:3},{value:"\u5C55\u793A\u5143\u7D20\u6587\u672C\u7684\u5B57\u6BB5\uFF0C\u4E0D\u9009\u5219\u5C55\u793A\u5143\u7D20\u5E8F\u53F7",paraId:5,tocIndex:3},{value:"string[] &#124; undefined",paraId:5,tocIndex:3},{value:"undefined",paraId:5,tocIndex:3},{value:"wasmPath",paraId:5,tocIndex:3},{value:"sam \u7EC4\u4EF6\u7684 wasm \u8DEF\u5F84",paraId:5,tocIndex:3},{value:"string",paraId:5,tocIndex:3},{value:"\\",paraId:5,tocIndex:3},{value:"showDrawDistance",paraId:5,tocIndex:3},{value:"\u7ED8\u5236\u65F6\u662F\u5426\u5C55\u793A\u8DDD\u79BB\u6587\u672C",paraId:5,tocIndex:3},{value:"boolean",paraId:5,tocIndex:3},{value:"false",paraId:5,tocIndex:3},{value:"showDrawArea",paraId:5,tocIndex:3},{value:"\u7ED8\u5236\u65F6\u662F\u5426\u5C55\u793A\u9762\u79EF\u6587\u672C",paraId:5,tocIndex:3},{value:"boolean",paraId:5,tocIndex:3},{value:"false",paraId:5,tocIndex:3},{value:"customTiles",paraId:5,tocIndex:3},{value:"\u81EA\u5B9A\u4E49\u74E6\u7247\u5E95\u56FE\u56FE\u5C42",paraId:5,tocIndex:3},{value:"CustomTilesProps",paraId:8,tocIndex:3},{value:"[]",paraId:5,tocIndex:3},{value:"tabItems",paraId:3},{value:"\u4FA7\u9762\u7248\u6807\u7B7E\u9875\u9009\u9879\u5361\u5185\u5BB9\uFF0C\u53EF\u4EE5\u81EA\u5B9A\u4E49\u62D3\u5C55\u7EC4\u4EF6",paraId:9,tocIndex:4},{value:"\u642D\u914D",paraId:10,tocIndex:5},{value:"tabItems",paraId:10,tocIndex:5},{value:"\u4E00\u8D77\u4F7F\u7528\uFF0C\u7528\u6765\u5C55\u793A\u53F3\u4FA7\u9762\u677F\u529F\u80FD",paraId:10,tocIndex:5},{value:"GeoJSON \u6587\u672C\u7F16\u8F91\u5668\uFF0C\u53EF\u4EE5\u901A\u8FC7\u8F93\u5165 GeoJSON \u6570\u636E\u6765\u5B9E\u73B0\u6570\u636E\u5C55\u793A",paraId:11,tocIndex:6},{value:"\u8981\u4F7F\u7528\u8BE5\u7EC4\u4EF6\u5F97\u5728 webpack \u91CC\u9762\u914D\u7F6E \u4EE5\u4E0B\u8FD9\u6BB5\u4EE3\u7801 \u5426\u5219\u5C31\u4F1A\u5BFC\u81F4\u4EE3\u7801\u683C\u5F0F\u5316\u5931\u6548\uFF0C\u4EE3\u7801\u9AD8\u4EAE\u4E22\u5931",paraId:12,tocIndex:6},{value:`import MonacoEditorWebpackPlugin from 'monaco-editor-webpack-plugin';
+
+  chainWebpack: (config: any) => {
+    config.plugin('monaco-editor').use(MonacoEditorWebpackPlugin, [
+      {
+        languages: ['json'],
+      },
+    ]);
+  },
+`,paraId:13,tocIndex:6},{value:"WKT \u6587\u672C\u7F16\u8F91\u5668\uFF0C\u53EF\u4EE5\u901A\u8FC7\u8F93\u5165 WKT \u6570\u636E\u6765\u5B9E\u73B0\u6570\u636E\u5C55\u793A",paraId:14,tocIndex:7},{value:"LngLat \u6587\u672C\u7F16\u8F91\u5668\uFF0C\u53EF\u4EE5\u901A\u8FC7\u8F93\u5165 LngLat \u6570\u636E\u5B9E\u73B0\u6570\u636E\u5C55\u793A(\u76EE\u524D\u53EA\u652F\u6301\u70B9\u6570\u636E\u5C55\u793A)",paraId:15,tocIndex:8},{value:"\u5FEB\u901F\u5C55\u793A GeoJSON \u6570\u636E\u4E2D\u7684 properties \u6570\u636E \uFF0C\u540C\u65F6\u4E5F\u53EF\u4EE5\u901A\u8FC7\u8868\u683C\u6765\u5C55\u793A\u4FEE\u6539\u5220\u9664 GeoJSON \u6570\u636E\u4E2D\u7684 properties \u5BF9\u8C61\u4E2D\u7684\u5B57\u6BB5",paraId:16,tocIndex:9},{value:`{
+  tabItems: [
+    {
+      key: 'geojson',
+      label: 'GeoJSON',
+      children: <GeoJsonEditor />,
+    },
+    {
+      key: 'table',
+      label: '\u8868\u683C',
+      children: <AppTable />,
+    },
+  ];
+}
+`,paraId:17,tocIndex:9},{value:`{
+  customTiles: [
+    {
+      id: 'GaodeSatellite',
+      image:
+        'https://mdn.alipayobjects.com/huamei_k6sfo0/afts/img/A*zi2jSqqZ2-8AAAAAAAAAAAAADjWqAQ/original',
+      title: '\u9AD8\u5FB7\u536B\u661F\u5E95\u56FE',
+      layers: [
+        'https://webst01.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}',
+      ],
+    },
+  ];
+}
+`,paraId:18,tocIndex:10},{value:"\u5C5E\u6027",paraId:19,tocIndex:10},{value:"\u63CF\u8FF0",paraId:19,tocIndex:10},{value:"\u7C7B\u578B",paraId:19,tocIndex:10},{value:"id",paraId:19,tocIndex:10},{value:"\u552F\u4E00\u6807\u8BC6",paraId:19,tocIndex:10},{value:"string",paraId:19,tocIndex:10},{value:"image",paraId:19,tocIndex:10},{value:"\u56FE\u7247",paraId:19,tocIndex:10},{value:"string",paraId:19,tocIndex:10},{value:"title",paraId:19,tocIndex:10},{value:"\u56FE\u5C42\u540D\u79F0",paraId:19,tocIndex:10},{value:"string",paraId:19,tocIndex:10},{value:"layers",paraId:19,tocIndex:10},{value:"\u74E6\u7247\u56FE\u5C42\u94FE\u63A5",paraId:19,tocIndex:10},{value:"string[]",paraId:19,tocIndex:10},{value:"\u5C5E\u6027",paraId:20,tocIndex:11},{value:"\u63CF\u8FF0",paraId:20,tocIndex:11},{value:"GCJ02",paraId:20,tocIndex:11},{value:"WGS84==>GCJ02",paraId:20,tocIndex:11},{value:"WGS84",paraId:20,tocIndex:11},{value:"GCJ02==>WGS84",paraId:20,tocIndex:11},{value:"\u63A7\u5236\u663E\u793A\u9690\u85CF\u5730\u56FE\u63A7\u4EF6",paraId:21,tocIndex:12},{value:`{
+  mapControl = {{drawControl: false}}
+}
+`,paraId:22,tocIndex:12},{value:"\u5C5E\u6027",paraId:23,tocIndex:13},{value:"\u63CF\u8FF0",paraId:23,tocIndex:13},{value:"drawControl",paraId:23,tocIndex:13},{value:"\u7ED8\u5236\u63A7\u4EF6",paraId:23,tocIndex:13},{value:"clearControl",paraId:23,tocIndex:13},{value:"\u6E05\u9664\u63A7\u4EF6",paraId:23,tocIndex:13},{value:"zoomControl",paraId:23,tocIndex:13},{value:"\u7F29\u653E\u5668\u63A7\u4EF6",paraId:23,tocIndex:13},{value:"scaleControl",paraId:23,tocIndex:13},{value:"\u6BD4\u4F8B\u5C3A\u63A7\u4EF6",paraId:23,tocIndex:13},{value:"locationSearchControl",paraId:23,tocIndex:13},{value:"\u5730\u70B9\u641C\u7D22\u63A7\u4EF6",paraId:23,tocIndex:13},{value:"mouseLocationControl",paraId:23,tocIndex:13},{value:"\u5149\u6807\u7ECF\u7EAC\u5EA6\u63A7\u4EF6",paraId:23,tocIndex:13},{value:"filterControl",paraId:23,tocIndex:13},{value:"\u6570\u636E\u8FC7\u6EE4\u63A7\u4EF6",paraId:23,tocIndex:13},{value:"officialLayerControl",paraId:23,tocIndex:13},{value:"\u5B98\u65B9\u56FE\u5C42\u63A7\u5236\u63A7\u4EF6",paraId:23,tocIndex:13},{value:"mapThemeControl",paraId:23,tocIndex:13},{value:"\u56FE\u5C42\u4E3B\u9898\u63A7\u4EF6",paraId:23,tocIndex:13},{value:"geoLocateControl",paraId:23,tocIndex:13},{value:"\u5B9A\u4F4D\u63A7\u4EF6",paraId:23,tocIndex:13},{value:"layerColorControl",paraId:23,tocIndex:13},{value:"\u56FE\u5C42\u989C\u8272\u63A7\u5236\u63A7\u4EF6",paraId:23,tocIndex:13},{value:"autoControl",paraId:23,tocIndex:13},{value:"\u81EA\u9002\u5E94\u63A7\u4EF6",paraId:23,tocIndex:13},{value:"fullscreenControl",paraId:23,tocIndex:13},{value:"\u5168\u5C4F\u63A7\u4EF6",paraId:23,tocIndex:13},{value:"administrativeSelectControl",paraId:23,tocIndex:13},{value:"\u884C\u653F\u533A\u57DF\u9009\u62E9\u63A7\u4EF6",paraId:23,tocIndex:13},{value:"mapAdministrativeControl",paraId:23,tocIndex:13},{value:"\u67E5\u770B\u5F53\u524D\u884C\u653F\u533A\u57DF\u63A7\u4EF6",paraId:23,tocIndex:13},{value:"logoControl",paraId:23,tocIndex:13},{value:"Logo \u63A7\u4EF6",paraId:23,tocIndex:13},{value:"textLayerControl",paraId:23,tocIndex:13},{value:"\u6587\u672C\u56FE\u5C42 \u63A7\u4EF6",paraId:23,tocIndex:13},{value:"exportImageControl",paraId:23,tocIndex:13},{value:"\u5BFC\u51FA\u56FE\u7247 \u63A7\u4EF6",paraId:23,tocIndex:13},{value:"\u63A7\u5236\u663E\u793A\u9690\u85CF\u5934\u90E8\u7EC4\u4EF6",paraId:24,tocIndex:14},{value:`{
+  toolbar = {{logo: false}}
+}
+`,paraId:25,tocIndex:14},{value:"\u5C5E\u6027",paraId:26,tocIndex:15},{value:"\u63CF\u8FF0",paraId:26,tocIndex:15},{value:"logo",paraId:26,tocIndex:15},{value:"logo",paraId:26,tocIndex:15},{value:"baseMap",paraId:26,tocIndex:15},{value:"\u5E95\u56FE\u5207\u6362",paraId:26,tocIndex:15},{value:"import",paraId:26,tocIndex:15},{value:"\u4E0A\u4F20",paraId:26,tocIndex:15},{value:"download",paraId:26,tocIndex:15},{value:"\u4E0B\u8F7D",paraId:26,tocIndex:15},{value:"guide",paraId:26,tocIndex:15},{value:"\u5F15\u5BFC",paraId:26,tocIndex:15},{value:"help",paraId:26,tocIndex:15},{value:"\u5E2E\u52A9",paraId:26,tocIndex:15},{value:"setting",paraId:26,tocIndex:15},{value:"\u8BBE\u7F6E",paraId:26,tocIndex:15},{value:"theme",paraId:26,tocIndex:15},{value:"\u4E3B\u9898",paraId:26,tocIndex:15},{value:"dingTalk",paraId:26,tocIndex:15},{value:"\u9489\u9489\u7FA4",paraId:26,tocIndex:15},{value:"i18n",paraId:26,tocIndex:15},{value:"\u8BED\u8A00\u5207\u6362",paraId:26,tocIndex:15},{value:"onFeatureChange",paraId:3},{value:"\u6570\u636E\u53D8\u5316\u65F6\u7684\u76D1\u542C\u65B9\u6CD5",paraId:27,tocIndex:16},{value:`    onFeatureChange={(features: Feature[]) => {}}
+`,paraId:28,tocIndex:16}]},70332:function(o,e,a){a.r(e),a.d(e,{texts:function(){return n}});const n=[]},2687:function(o,e,a){a.r(e),a.d(e,{texts:function(){return n}});const n=[]},34676:function(o,e){e.Z=`.editor {
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  left: 0;
+  top: 0;
+  z-index: 100;
+  background-color: #fff;
+}
+
+.dumi-default-content {
+  min-height: calc(100vh - 130px) !important;
+}`},323:function(o,e){e.Z=`import { L7Editor } from '@antv/l7-editor';
+import React from 'react';
+import './index.less';
+
+export default () => {
+  return (
+    <div className="editor">
+      <L7Editor activeTab="geojson" />
+    </div>
+  );
+};
+`},27786:function(o,e){e.Z=`import { L7Editor } from '@antv/l7-editor';
+
+import React from 'react';
+
+export default () => {
+  return (
+    <div style={{ height: 800 }}>
+      <L7Editor />
+    </div>
+  );
+};
+`}}]);
